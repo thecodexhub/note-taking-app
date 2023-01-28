de84a505e19ffa5bd461d9c475158e93a36d27ca
Sandip <sandipramanik07@gmail.com>
2023-01-28 16:18:17 +0530

    chore(authentication_repository): create new package

---
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -0,0 +1,4 @@
+/// Dart package that manages the authentication domain
+library authentication_repository;
+
+export 'src/authentication_repository.dart';
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Dart package that manages the authentication domain
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
