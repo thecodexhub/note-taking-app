b73fd6e4c32f13916e7cf74ae805b0e881d09a00
Sandip <sandipramanik07@gmail.com>
2023-01-29 23:22:03 +0530

    feat(authentication_repository): add login methods (magic link)

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,7 +1,162 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:meta/meta.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase_flutter;
+
+/// {@template sign_in_with_magic_link_failure}
+/// Thrown during sign in with magic link flow if any failure occurs.
+/// {@endtemplate}
+class SignInWithMagicLinkFailure implements Exception {
+  /// {@macro sign_in_with_magic_link_failure}
+  const SignInWithMagicLinkFailure({
+    this.message = 'An unknown error occurred.',
+  });
+
+  /// The associated error message
+  final String message;
+}
+
+/// Thrown during the logout process if a failure occurs.
+class SignOutFailure implements Exception {}
+
+/// {@template update_profile_failure}
+/// Thrown during profile update if any failure occurs.
+/// {@endtemplate}
+class UpdateProfileFailure implements Exception {
+  /// {@macro update_profile_failure}
+  const UpdateProfileFailure({
+    this.message = 'An unknown error occurred.',
+  });
+
+  /// The associated error message
+  final String message;
+}
+
+/// {@template fetch_profile_failure}
+/// Thrown during profile fetch if any failure occurs.
+/// {@endtemplate}
+class FetchProfileFailure implements Exception {
+  /// {@macro fetch_profile_failure}
+  const FetchProfileFailure({
+    this.message = 'An unknown error occurred.',
+  });
+
+  /// The associated error message
+  final String message;
+}
+
 /// {@template authentication_repository}
-/// Dart package that manages the authentication domain
+/// Repository that manages the user authentication.
 /// {@endtemplate}
 class AuthenticationRepository {
   /// {@macro authentication_repository}
-  const AuthenticationRepository();
+  AuthenticationRepository({
+    required supabase_flutter.SupabaseClient client,
+  }) : _client = client;
+
+  final supabase_flutter.SupabaseClient _client;
+
+  /// Whether or not the current environment is Web.
+  /// Should only be overriden for testing purpose. Otherwise,
+  /// defaults to [kIsWeb].
+  @visibleForTesting
+  bool isWeb = kIsWeb;
+
+  /// URL that auth providers are permitted to redirect to post authentication.
+  /// Should only be used for testing purposes.
+  @visibleForTesting
+  static const redirectUrl = 'io.supabase.flutterquickstart://login-callback/';
+
+  /// Profiles table name.
+  @visibleForTesting
+  static const profilesTable = 'profiles';
+
+  /// Stream of [User] which will emit the current user when
+  /// the authentication state changes.
+  ///
+  /// Emits [User.empty] if the user is not authenticated.
+  Stream<User> get user {
+    return _client.auth.onAuthStateChange.map((authState) {
+      final session = authState.session;
+      final user = session == null ? User.empty : session.user.toUser;
+      return user;
+    });
+  }
+
+  /// Returns the current user
+  /// Defaults to [User.empty] if there is no current user.
+  User get currentUser {
+    return _client.auth.currentUser?.toUser ?? User.empty;
+  }
+
+  /// Creates the login flow with the magiclink
+  ///
+  /// Throws a [SignInWithMagicLinkFailure] if any exception occurs.
+  Future<void> signInWithMagicLink({required String email}) async {
+    try {
+      await _client.auth.signInWithOtp(
+        email: email,
+        emailRedirectTo: isWeb ? null : redirectUrl,
+      );
+    } on supabase_flutter.AuthException catch (error) {
+      throw SignInWithMagicLinkFailure(message: error.message);
+    } catch (_) {
+      throw const SignInWithMagicLinkFailure();
+    }
+  }
+
+  /// Updates user's information into the `profiles` table.
+  Future<void> updateProfile({
+    required String username,
+    required String fullName,
+  }) async {
+    final updates = {
+      'id': currentUser.id,
+      'username': username,
+      'full_name': fullName,
+      'updated_at': DateTime.now().toIso8601String(),
+    };
+
+    try {
+      await _client.from(profilesTable).upsert(updates);
+    } on supabase_flutter.PostgrestException catch (error) {
+      throw UpdateProfileFailure(message: error.message);
+    } catch (_) {
+      throw const UpdateProfileFailure();
+    }
+  }
+
+  /// Fetches user's information from the `profiles` table.
+  Future<User> getProfile() async {
+    try {
+      final profile = await _client
+          .from(profilesTable)
+          .select<supabase_flutter.PostgrestMap>()
+          .eq('id', currentUser.id)
+          .single();
+      return User.fromMap(profile).copyWith(email: currentUser.email);
+    } on supabase_flutter.PostgrestException catch (error) {
+      throw FetchProfileFailure(message: error.message);
+    } catch (_) {
+      throw const FetchProfileFailure();
+    }
+  }
+
+  /// Signs out the current user which will emit
+  /// [User.empty] from the [user] Stream.
+  ///
+  /// Throws a [SignOutFailure] if an exception occurs.
+  Future<void> signOut() async {
+    try {
+      await _client.auth.signOut();
+    } catch (_) {
+      throw SignOutFailure();
+    }
+  }
+}
+
+extension on supabase_flutter.User {
+  User get toUser {
+    return User(id: id, email: email);
+  }
 }
---
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: sort_constructors_first
+
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+/// {@template user}
+/// User model.
+///
+/// [User.empty] represents an unauthenticated user.
+/// {@endtemplate}
+class User extends Equatable {
+  /// {@macro user}
+  const User({
+    this.email,
+    required this.id,
+    this.username,
+    this.fullName,
+    this.avatarUrl,
+    this.updatedAt,
+  });
+
+  /// The current user's email.
+  final String? email;
+
+  /// The current user's ID.
+  final String id;
+
+  /// The current user's username.
+  final String? username;
+
+  /// The current user's full name.
+  final String? fullName;
+
+  /// Url for the photo of the current user.
+  final String? avatarUrl;
+
+  /// Last time when the user was updated.
+  final DateTime? updatedAt;
+
+  /// Empty user which represents an unauthenticated user.
+  static const empty = User(id: '');
+
+  /// Convenient getter to determine whether the current user is empty.
+  bool get isEmpty => this == User.empty;
+
+  /// Convenient getter to determine whether the current user is not empty.
+  bool get isNotEmpty => this != User.empty;
+
+  @override
+  List<Object?> get props =>
+      [email, id, username, fullName, avatarUrl, updatedAt];
+
+  /// Creates a copy of this class with updated values.
+  ///
+  /// {@macro user}
+  User copyWith({
+    String? email,
+    String? id,
+    String? username,
+    String? fullName,
+    String? avatarUrl,
+    DateTime? updatedAt,
+  }) {
+    return User(
+      email: email ?? this.email,
+      id: id ?? this.id,
+      username: username ?? this.username,
+      fullName: fullName ?? this.fullName,
+      avatarUrl: avatarUrl ?? this.avatarUrl,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+
+  /// Creates User from a map object
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: (map['id'] ?? '') as String,
+      fullName: map['fullName'] as String,
+      username: map['username'] as String,
+      avatarUrl: map['avatarUrl'] as String,
+      updatedAt: map['updatedAt'] != null
+          ? DateTime.parse(map['updatedAt'] as String)
+          : null,
+    );
+  }
+}
